887be35b05abc35d464b69eaa8c540d3c55187ef
BURAK KEBAPCI <burakglobal@gmail.com>
2018-06-06 11:28:36 -0400

    Update Example1.swift

--- Example1.swift
+++ Example1.swift
@@ -38,3 +38,20 @@ func example4(manatees: [[String : Any]]) {
     }
     print(oldestManatee)
 }
+
+
+// What is the efficiency of Example 1 in Big-O notation?
+// We iterate over every manatee in the manatees list with the for loop. Since we're given that manatees has n elements, our code will take approximately O(n) time to run.
+
+
+// What is the efficiency of Example 2 in Big-O notation?
+// We look at two specific properties of a specific manatee. We aren't iterating over anything - just doing constant-time lookups on lists and dictionaries. Thus the code will complete in constant, or O(1), time.
+
+// What is the efficiency of Example 3 in Big-O notation?
+// There are two for loops, and nested for loops are a good sign that you need to multiply two runtimes. Here, for every manatee, we check every property. If we had 4 manatees, each with 5 properties, then we would need 5+5+5+5 steps. This logic simplifies to the number of manatees times the number of properties, or O(nm).
+
+// What is the efficiency of Example 4 in Big-O notation?
+// Again we have nested for loops. This time we're iterating over the manatees list twice - every time we see a manatee, we compare it to every other manatee's age. We end up with O(nn), or O(n^2) (which is read as "n squared").
+
+
+
